b00635653e41b09730932a3ac44bf3de18929475
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-22 19:42:05 +0100

    added an error handling by using json method from react-router-dom and simplyfied a throwing New Response in JSON format

--- frontend/src/Pages/Events.js
+++ frontend/src/Pages/Events.js
@@ -1,14 +1,15 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, json } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
 	const data = useLoaderData();
-	const events = data.events;
 
 	// if (data.isError) {
 	// 	return <p>{data.message}</p>;
 	// }
+
+	const events = data.events;
 
 	return <EventsList events={events} />;
 }
@@ -16,11 +17,11 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-	const response = await fetch('http://localhost:8080/events');
+	const response = await fetch('http://localhost:8080/event');
 
 	if (!response.ok) {
-		// return { isError: true, message: 'Could not fetch events.' };
-		throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), { status: 500 });
+		// throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), { status: 500 });
+		throw json({ message: 'Could not fetch events.' }, { status: 500 });
 	} else {
 		return response;
 	}
--- frontend/src/Pages/Root.js
+++ frontend/src/Pages/Root.js
@@ -1,16 +1,12 @@
 import { Outlet } from 'react-router-dom';
-// import { useNavigation } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 export const RootLayout = () => {
-	// const navigation = useNavigation();
-
 	return (
 		<>
 			<MainNavigation />
 			<main>
-				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
 				<Outlet />
 			</main>
 		</>
